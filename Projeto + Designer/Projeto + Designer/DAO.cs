181d017893c0b955c62cca2c06c8c00b45c23dc4
nicole.goncalves6@etec.sp.gov.br <nicole.goncalves6@etec.sp.gov.br>
2024-06-15 13:11:38 -0300

    att DAO

--- Projeto + Designer/Projeto + Designer/DAO.cs
+++ Projeto + Designer/Projeto + Designer/DAO.cs
@@ -1,6 +1,7 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -10,9 +11,10 @@ namespace Projeto___Designer
 {
     public class DAO
     {
+
         MySqlConnection conn;
         MySqlCommand cmd;
-        string connection = "server=localhost; port=3306; database=gerenciamento; uid=root; pwd='FuscaAzuL123.'";
+        string connection = "server=localhost; port=3306; database=Gerenciamento; uid=root; pwd='FuscaAzuL123.'";
 
         public void Conectar()
         {
@@ -39,8 +41,47 @@ namespace Projeto___Designer
             return cmd.ExecuteReader();
         }
 
+        public DataTable GetLivros()
+        {
+            Conectar();
+            string query = "SELECT Id, Nome FROM Livros_Emprestimos";
+            MySqlDataAdapter adapter = new MySqlDataAdapter(query, conn);
+            DataTable livros = new DataTable();
+            adapter.Fill(livros);
+            Desconectar();
+            return livros;
+        }
 
+        public DataTable BuscarLivros(string filtro)
+        {
+            Conectar();
+            string query = "SELECT Id, Nome FROM Livros_Emprestimos WHERE Nome LIKE @Filtro";
+            MySqlDataAdapter adapter = new MySqlDataAdapter(query, conn);
+            adapter.SelectCommand.Parameters.AddWithValue("@Filtro", "%" + filtro + "%");
+            DataTable livros = new DataTable();
+            adapter.Fill(livros);
+            Desconectar();
+            return livros;
+        }
+
+        public void SolicitarEmprestimo(int idUsuario, int idLivro)
+        {
+            Conectar();
+            string query = "INSERT INTO Solicitacao (idUsuario, idLivro, dataDeSolicitacao, statusDaSolicitacao, dataDeDevolucao) " +
+                           "VALUES (@idUsuario, @idLivro, @dataDeSolicitacao, 'pendente', @dataDeDevolucao)";
+            using (cmd = new MySqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@idUsuario", idUsuario);
+                cmd.Parameters.AddWithValue("@idLivro", idLivro);
+                cmd.Parameters.AddWithValue("@dataDeSolicitacao", DateTime.Now);
+                cmd.Parameters.AddWithValue("@dataDeDevolucao", DateTime.Now.AddDays(14));
+                cmd.ExecuteNonQuery();
+            }
+            Desconectar();
+        }
     }
+
 }
+
 
 
